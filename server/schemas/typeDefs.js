b5ab87ac8f585840336cccd279204eb78f6e0f19
Punkinut <heypunkinut@gmail.com>
2021-07-22 11:18:14 +1000

    Added two mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -26,6 +26,11 @@ const typeDefs = gql`
  type Query {
      me: User
  }
+
+ type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+ }
 `;
 
 module.exports = typeDefs
