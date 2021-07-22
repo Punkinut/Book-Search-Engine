465a5e4a6b9fcb62313120c32d0fe602f7c57497
Punkinut <heypunkinut@gmail.com>
2021-07-22 11:28:15 +1000

    Added me query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,14 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-      placeHolder: async () => {
-        return User.find({});
+      me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id }).populate('savedBooks');
+        }
+        throw new AuthenticationError('Cannot find a user with this id!');
       },
     },
   };
