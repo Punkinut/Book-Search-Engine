5b114da8961a0f1671439a639311af22c95807ea
Punkinut <heypunkinut@gmail.com>
2021-07-23 12:47:45 +1000

    You are now able to login using apollo server

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,6 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
@@ -29,23 +28,11 @@ const LoginForm = () => {
     }
 
     try {
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
 
-      // const { data } = await login({
-      //   variables: { ...userFormData },
-      // });
-
-      // Auth.login(data.login.token);
-
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-
-      Auth.login(token);
-
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
