755b5cfc6a6342bffc7c142c016979832e5a80e3
Punkinut <heypunkinut@gmail.com>
2021-07-23 11:38:59 +1000

    Added front end login and add user mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,45 @@
 import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          user {
+            _id
+            email
+          }
+          token
+        }
+      }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+          user {
+            _id
+            email
+          }
+          token
+        }
+      }
+`;
+
+// export const SAVE_BOOK = gql`
+//   mutation createMatchup($tech1: String!, $tech2: String!) {
+//     createMatchup(tech1: $tech1, tech2: $tech2) {
+//       _id
+//       tech1
+//       tech2
+//     }
+//   }
+// `;
+
+// export const REMOVE_BOOK = gql`
+//   mutation createMatchup($tech1: String!, $tech2: String!) {
+//     createMatchup(tech1: $tech1, tech2: $tech2) {
+//       _id
+//       tech1
+//       tech2
+//     }
+//   }
+// `;
